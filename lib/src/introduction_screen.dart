f20df381d9a6c073344b9ba84bf975cd0176c758
Sharad Chand <pepsighan@sharadchand.com>
2021-01-09 15:12:17 +0545

    Add pedantic linter and increase the env constraints

--- lib/src/introduction_screen.dart
+++ lib/src/introduction_screen.dart
@@ -104,7 +104,6 @@ class IntroductionScreen extends StatefulWidget {
   /// Color of done button
   final Color doneColor;
 
-
   const IntroductionScreen({
     Key key,
     @required this.pages,
@@ -130,7 +129,7 @@ class IntroductionScreen extends StatefulWidget {
     this.color,
     this.skipColor,
     this.nextColor,
-    this.doneColor
+    this.doneColor,
   })  : assert(pages != null),
         assert(
           pages.length > 0,
